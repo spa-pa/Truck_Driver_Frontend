e07fd023bb7300bb4a0bd428c2a15657252f7de6
spa-pa <sandeshaher11@gmail.com>
2026-06-20 16:03:39 +0530

    feat: Update language service endpoints and adjust language config keys

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 const version = 'api/v1/';
-// const domain = 'localhost:3000/';
+const domain = 'localhost:3000/';
 
-const domain = 'truck-driver-backend-dpa1.onrender.com/';
+// const domain = 'truck-driver-backend-dpa1.onrender.com/';
 
 export const environment = {
     production: false,
     ENABLE_ENCRYPTION: false,
-    API_BASE_URL: 'https://' + domain + version,
+    API_BASE_URL: 'http://' + domain + version,
 };
